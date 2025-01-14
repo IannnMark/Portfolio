051074259f18e99cdc52bfbf4d79339a97b011f3
ianmark8 <ianmarkmorga07@gmail.com>
2025-01-14 20:46:40 +0800

    Fix image import path and integrate ParticleBackground component in Home

--- src/content/Home.jsx
+++ src/content/Home.jsx
@@ -1,15 +1,17 @@
-import image from "../../public/images/Pic.JPG";
+import image from "../../images/Pic.JPG";
+import ParticleBackground from "./ParticleBackground";
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col items-center gap-1 py-10 sm:py-10 max-w-6xl mx-auto">
+      <div className="flex flex-col items-center py-10 sm:py-10 max-w-6xl mx-auto">
+        <ParticleBackground />
         <img
           src={image}
           alt="Pic"
           className="h-20 w-20 rounded-full mt-20 cursor-pointer"
         />
 
-        <h1 className=" text-white font-medium text-[30px] sm:text-[70px] md:text-[100px] whitespace-nowrap text-center font-sans">
+        <h1 className=" text-white font-medium text-[30px] sm:text-[70px] md:text-[100px] whitespace-nowrap text-center font-sans m-0 leading-tight">
           <span className="whitespace-nowrap">Building websites</span>
           <br />
           <span className="block">that deliver results</span>
