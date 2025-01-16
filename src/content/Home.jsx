73291528b81471338d61c79dd31bcc7a945bf590
ianmark8 <ianmarkmorga07@gmail.com>
2025-01-16 20:50:10 +0800

    Add personal introduction and contact section to Home component

--- src/content/Home.jsx
+++ src/content/Home.jsx
@@ -188,6 +188,47 @@ export default function Home() {
         </div>
       </div>
 
+      <div className="flex flex-col md:flex-row justify-center md:p-16">
+        <div className="max-w-lg p-10 md:flex-row">
+          <h1 className="m-0 leading-tight text-white font-medium text-[60px] my-10 text-left">
+            <span className="whitespace-nowrap">Bringing ideas to life</span>
+            <br />
+            <span className="block">through code</span>
+          </h1>
+          <p className="text-white font-normal text-[14px] md:text-[18px] text-left">
+            Hi, I’m Ian! I’m a full-stack developer with 2 years of experience
+            creating reliable, scalable, and innovative web solutions. I
+            specialize in both front-end and back-end development, blending
+            functionality with great user experiences.
+          </p>
+
+          <p className="text-white font-normal text-[14px] md:text-[18px] my-7 text-left">
+            Outside of work, I enjoy playing games and watching videos, which
+            often inspire new ideas and keep me connected to the latest trends
+            in tech specifically in web development
+          </p>
+        </div>
+        <div
+          className="w-[327px] h-[403px] md:w-[547px] md:h-[733px] rounded-lg"
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: "cover",
+            backgroundPosition: "bottom",
+          }}
+        ></div>
+      </div>
+
+      <div className="bg-neutral-700 w-[1237px] h-[162px] flex items-center justify-center mx-auto rounded-lg">
+        <div className="flex items-center justify-center gap-7">
+          <p className="text-neutral-300">
+            Interested in working with me? I’d love to hear from you!
+          </p>
+
+          <p className="text-neutral-700 bg-white rounded-lg px-4 py-4">
+            ianmarkmorga07@gmail.com
+          </p>
+        </div>
+      </div>
 
       
     </div>
