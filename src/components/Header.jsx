5dbda8c74e2ff760607774a9270e9c7b65c0b509
ianmark8 <ianmarkmorga07@gmail.com>
2025-01-21 14:05:29 +0800

    Add mobile menu toggle to Header component for improved navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import Logo from "../../images/color_white_primary_logo.png";
+import { GrClose } from "react-icons/gr";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <header className="bg-black shadow-lg py-4">
       <div className="container mx-auto flex items-center justify-between px-4 md:px-6 lg:px-8 border border-gray-600 rounded-lg py-3">
@@ -40,9 +44,60 @@ export default function Header() {
         </div>
 
         <div>
-          <button className="bg-white text-black p-2 px-5 rounded-lg border border-black text-[16px] font-normal hover:scale-105 transition duration-300">
-            Get in touch
+          <a href="#contact">
+            <button className="hidden sm:flex bg-white text-black p-2 px-5 rounded-lg border border-black text-[16px] font-normal hover:scale-105 transition duration-300">
+              Get in touch
+            </button>
+          </a>
+        </div>
+        <div className="relative sm:hidden">
+          <button
+            onClick={() => setShowMenu(!showMenu)}
+            className="font-bold text-white text-xl w-[14px] h-[16px] hover:shadow-lg flex-shrink-0 transition duration-300 hover:text-neutral-500 hover:scale-105"
+          >
+            {showMenu ? <GrClose /> : <GiHamburgerMenu />}
           </button>
+
+          {showMenu && (
+            <div
+              className="absolute top-10 right-0 bg-white shadow-current flex flex-col items-start p-4 gap-3 z-10 w-48"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <a
+                href="#Projects"
+                className="uppercase text-black hover:shadow-current transition duration-300 hover:scale-105"
+                onClick={() => setShowMenu(false)}
+              >
+                Projects
+              </a>
+              <a
+                href="#about"
+                className="uppercase text-black hover:shadow-current transition duration-300 hover:scale-105"
+                onClick={() => setShowMenu(false)}
+              >
+                About
+              </a>
+              <a
+                href="#contact"
+                className="uppercase text-black hover:shadow-current transition duration-300 hover:scale-105"
+                onClick={() => setShowMenu(false)}
+              >
+                Contact
+              </a>
+              <a
+                href=""
+                className="uppercase text-black hover:shadow-current transition duration-300 hover:scale-105"
+              >
+                CV
+              </a>
+              <a
+                href="#contact"
+                className="uppercase text-black hover:shadow-current transition duration-300 hover:scale-105"
+              >
+                Get in touch
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </header>
